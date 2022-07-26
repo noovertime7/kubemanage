c02320f212a0764a320dec9c7d879a0822a530bb
chenteng <cc17854330572@163.com>
2022-07-26 09:17:54 +0800

    style: 代码风格优化

--- service/init.go
+++ service/init.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"flag"
+	"github.com/wonderivan/logger"
 	"os"
 	"path/filepath"
 
@@ -19,29 +20,30 @@ type k8s struct {
 func (k *k8s) Init() {
 	var err error
 	var config *rest.Config
-	var kubeconfig *string
+	var kubeConfig *string
 
 	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		kubeConfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		kubeConfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
 
 	// 使用 ServiceAccount 创建集群配置（InCluster模式）
 	if config, err = rest.InClusterConfig(); err != nil {
 		// 使用 KubeConfig 文件创建集群配置
-		if config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig); err != nil {
+		if config, err = clientcmd.BuildConfigFromFlags("", *kubeConfig); err != nil {
 			panic(err.Error())
 		}
 	}
 
-	// 创建 clientset
-	clientset, err := kubernetes.NewForConfig(config)
+	// 创建 clientSet
+	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
-	k.clientSet = clientset
+	logger.Info("获取clientSet成功")
+	k.clientSet = clientSet
 }
 
 func homeDir() string {
