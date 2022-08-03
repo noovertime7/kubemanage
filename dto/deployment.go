257105541e8522ffed272cc53678ed1b1ea6e0ca
chenteng <cc17854330572@163.com>
2022-08-03 09:07:33 +0800

    fix: Modify the DeployListInput field to be non-required

--- dto/deployment.go
+++ dto/deployment.go
@@ -25,7 +25,6 @@ type DeployCreateInput struct {
 }
 
 type UpdateDeployInput struct {
-	Name      string `json:"name" form:"name" comment:"无状态控制器名称" validate:"required"`
 	NameSpace string `json:"namespace" form:"namespace" comment:"命名空间" validate:"required"`
 	Content   string `json:"content" validate:"required" comment:"更新内容"`
 }
@@ -37,10 +36,10 @@ type DeployScaleInput struct {
 }
 
 type DeployListInput struct {
-	FilterName string `json:"filter_name" form:"filter_name" validate:"required" comment:"过滤名"`
-	NameSpace  string `json:"namespace" form:"namespace" validate:"required" comment:"命名空间"`
-	Limit      int    `json:"limit" form:"limit" validate:"required" comment:"分页限制"`
-	Page       int    `json:"page" form:"page" validate:"required" comment:"页码"`
+	FilterName string `json:"filter_name" form:"filter_name" validate:"" comment:"过滤名"`
+	NameSpace  string `json:"namespace" form:"namespace" validate:"" comment:"命名空间"`
+	Limit      int    `json:"limit" form:"limit" validate:"" comment:"分页限制"`
+	Page       int    `json:"page" form:"page" validate:"" comment:"页码"`
 }
 
 func (params *DeployListInput) BindingValidParams(c *gin.Context) error {
