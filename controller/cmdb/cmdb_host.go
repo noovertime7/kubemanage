298686ca1a868f209b0c14e900d772327e01a987
chenteng <cc17854330572@163.com>
2023-01-03 11:40:45 +0800

    feat(cmdb): 补充提交 #15

---
+++ controller/cmdb/cmdb_host.go
@@ -0,0 +1,86 @@
+package cmdb
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/noovertime7/kubemanage/dto"
+	"github.com/noovertime7/kubemanage/middleware"
+	v1 "github.com/noovertime7/kubemanage/pkg/core/kubemanage/v1"
+	"github.com/noovertime7/kubemanage/pkg/globalError"
+	"github.com/noovertime7/kubemanage/pkg/utils"
+)
+
+func (c *cmdbController) CreateHost(ctx *gin.Context) {
+	params := &dto.CMDBHostCreateInput{}
+	if err := params.BindingValidParams(ctx); err != nil {
+		v1.Log.ErrorWithCode(globalError.ParamBindError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.ParamBindError, err))
+		return
+	}
+	if err := v1.CoreV1.CMDB().Host().CreateHost(ctx, params); err != nil {
+		v1.Log.ErrorWithCode(globalError.CreateError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.CreateError, err))
+		return
+	}
+	middleware.ResponseSuccess(ctx, "")
+}
+
+func (c *cmdbController) UpdateHost(ctx *gin.Context) {
+	params := &dto.CMDBHostCreateInput{}
+	if err := params.BindingValidParams(ctx); err != nil {
+		v1.Log.ErrorWithCode(globalError.ParamBindError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.ParamBindError, err))
+		return
+	}
+	if err := v1.CoreV1.CMDB().Host().UpdateHost(ctx, params); err != nil {
+		v1.Log.ErrorWithCode(globalError.UpdateError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.UpdateError, err))
+		return
+	}
+	middleware.ResponseSuccess(ctx, "")
+}
+
+func (c *cmdbController) PageHost(ctx *gin.Context) {
+	params := &dto.PageListCMDBHostInput{}
+	if err := params.BindingValidParams(ctx); err != nil {
+		v1.Log.ErrorWithCode(globalError.ParamBindError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.ParamBindError, err))
+		return
+	}
+	data, err := v1.CoreV1.CMDB().Host().PageHost(ctx, params)
+	if err != nil {
+		v1.Log.ErrorWithCode(globalError.GetError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.GetError, err))
+		return
+	}
+	middleware.ResponseSuccess(ctx, data)
+}
+
+func (c *cmdbController) DeleteHost(ctx *gin.Context) {
+	instanceid, err := utils.ParseInt64(ctx.Param("instanceid"))
+	if err != nil {
+		v1.Log.ErrorWithCode(globalError.ParamBindError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.ParamBindError, err))
+	}
+	if err := v1.CoreV1.CMDB().Host().DeleteHost(ctx, instanceid); err != nil {
+		v1.Log.ErrorWithCode(globalError.DeleteError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.DeleteError, err))
+		return
+	}
+	middleware.ResponseSuccess(ctx, "")
+}
+
+func (c *cmdbController) DeleteHosts(ctx *gin.Context) {
+	params := &dto.IdsReq{}
+	if err := params.BindingValidParams(ctx); err != nil {
+		v1.Log.ErrorWithCode(globalError.ParamBindError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.ParamBindError, err))
+		return
+	}
+	if err := v1.CoreV1.CMDB().Host().DeleteHosts(ctx, params.Ids); err != nil {
+		v1.Log.ErrorWithCode(globalError.DeleteError, err)
+		middleware.ResponseError(ctx, globalError.NewGlobalError(globalError.DeleteError, err))
+		return
+	}
+	middleware.ResponseSuccess(ctx, "")
+}
