06c3f9b0498ab4a2f26dcf07eeaadc84f55ee61b
chenteng <cc17854330572@163.com>
2023-01-04 09:35:00 +0800

    feat(dao): add InstanceID unique;not null

--- dao/model/cmdb_host.go
+++ dao/model/cmdb_host.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"context"
+
 	"gorm.io/gorm"
 )
 
@@ -12,7 +13,7 @@ func init() {
 type CMDBHost struct {
 	Id              uint   `gorm:"column:id;primary_key;AUTO_INCREMENT;not null" json:"id"`
 	Name            string `json:"name" gorm:"index;column:name;comment:主机名"`
-	InstanceID      int64  `json:"instanceID" gorm:"index;column:instanceID;comment:唯一id"`
+	InstanceID      int64  `json:"instanceID" gorm:"unique;not null;index;column:instanceID;comment:唯一id"`
 	Address         string `json:"address" gorm:"column:address;comment:主机地址"`
 	Port            string `json:"port" gorm:"column:port;comment:主机端口"`
 	HostUserName    string `json:"hostUserName" gorm:"column:hostUserName;comment:主机用户名"`
